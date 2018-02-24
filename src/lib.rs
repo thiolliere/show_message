fd63fb708de66ebccc68119a1f97b5726b2a5354
thiolliere <gui.thiolliere@gmail.com>
2018-02-24 14:37:25 +0100

    windows do not use msg

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-use std::process::{Command, Stdio};
-use std::ffi::OsStr;
+#[cfg(target_os = "windows")]
+extern crate user32;
+#[cfg(target_os = "windows")]
+extern crate winapi;
 
 #[cfg(all(not(target_os = "windows"), not(target_os = "linux"), not(target_os = "dragonfly"),
   not(target_os = "freebsd"), not(target_os = "openbsd")))]
@@ -8,13 +10,13 @@ compile_error!("The platform you're compiling for is not supported by show messa
 pub trait SomeOrShow {
     type SomeType;
     fn some_or_show<M>(self, M) -> Self::SomeType
-    where M: AsRef<OsStr>;
+    where M: AsRef<str>;
 }
 
 impl<T> SomeOrShow for Option<T> {
     type SomeType = T;
     fn some_or_show<M>(self, msg: M) -> Self::SomeType
-    where M: AsRef<OsStr>
+    where M: AsRef<str>
     {
         match self {
             Some(t) => t,
@@ -51,25 +53,36 @@ impl<T, E> OkOrShow for Result<T, E> {
 
 #[cfg(target_os = "windows")]
 pub fn show<M>(message:  M)
-where M: AsRef<::std::ffi::OsStr>
+where M: AsRef<str>
 {
-    use std::io::Write;
+    use std::ffi::CString;
+    use std::ptr::null_mut;
+    use user32::MessageBoxA;
+    use winapi::winuser::{MB_OK, MB_SYSTEMMODAL};
 
-    let mut child = Command::new("msg")
-        .args(&[OsStr::new("*"), message.as_ref()])
-        .stdin(Stdio::null())
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .output()
-        .expect("failed to execute msg command");
+    let lp_text = CString::new(message).unwrap();
+    let lp_caption = CString::new("Message").unwrap();
+
+    let window_type = MB_OK | MB_SYSTEMMODAL;
+
+    unsafe {
+        MessageBoxA(
+            null_mut(),
+            lp_text.as_ptr(),
+            lp_caption.as_ptr(),
+            window_type
+        );
+    }
 }
 
 #[cfg(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"))]
 pub fn show<M>(message:  M)
-where M: AsRef<OsStr>
+where M: AsRef<str>
 {
+    use std::process::{Command, Stdio};
+
     Command::new("xmessage")
-        .args(&[message])
+        .args(&[message.as_ref()])
         .stdin(Stdio::null())
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
