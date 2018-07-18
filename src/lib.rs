466bcf250f8cfd2ce9ebf68d49d40dfb697ab6b6
thiolliere <gui.thiolliere@gmail.com>
2018-07-18 16:02:07 +0200

    impl default for other plateform

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,11 @@ extern crate winapi;
 
 #[cfg(all(not(target_os = "windows"), not(target_os = "linux"), not(target_os = "dragonfly"),
   not(target_os = "freebsd"), not(target_os = "openbsd"), not(target_os = "macos")))]
-compile_error!("The platform you're compiling for is not supported by show message");
+pub fn show<M>(message:  M)
+where M: AsRef<str>
+{
+    println!("{}", message.as_ref());
+}
 
 pub trait UnwrapOrShow: Sized {
     type OkType;
